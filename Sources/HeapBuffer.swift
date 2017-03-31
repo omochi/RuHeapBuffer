846b63b94fbeb6d71c60ed542a76c5eaada8d85c
omochi <omochi.metaru@gmail.com>
2017-04-01 00:48:43 +0900

    refactor

--- Sources/HeapBuffer.swift
+++ Sources/HeapBuffer.swift
@@ -7,40 +7,48 @@
 //
 
 public class HeapBuffer<T> {
-    public init(from: UnsafePointer<T>?, count: Int) {
+    public convenience init(pointer: UnsafePointer<T>?, count: Int) {
         if count == 0 {
-            buffer = UnsafeMutableBufferPointer<T>(start: nil, count: 0)
+            self.init(pointerNoCopy: nil, count: 0, freeWhenDone: true)
         } else {
-            let pointer = UnsafeMutablePointer<T>.allocate(capacity: count)
-            pointer.initialize(from: from!, count: count)
-            buffer = UnsafeMutableBufferPointer<T>(start: pointer, count: count)
+            let p = UnsafeMutablePointer<T>.allocate(capacity: count)
+            p.initialize(from: pointer!, count: count)
+            self.init(pointerNoCopy: p, count: count, freeWhenDone: true)
         }
+    }
+
+    public convenience init<C: Collection>(items: C)
+        where C.Iterator.Element == T
+    {
+        if items.count == 0 {
+            self.init(pointerNoCopy: nil, count: 0, freeWhenDone: true)
+        } else {
+            let count: Int = C.IndexDistance(0).distance(to: items.count)
+            let pointer = UnsafeMutablePointer<T>.allocate(capacity: count)
+            var p = pointer
+            var i = items.startIndex
+            while i < items.endIndex {
+                p.initialize(to: items[i])
+                p = p.advanced(by: 1)
+                i = items.index(after: i)
+            }
+            self.init(pointerNoCopy: pointer, count: count, freeWhenDone: true)
+        }
+    }
+
+    public init(pointerNoCopy: UnsafeMutablePointer<T>?, count: Int, freeWhenDone: Bool) {
+        self.pointer = pointerNoCopy
+        self.count = count
+        self.freeWhenDone = freeWhenDone
     }
 
     deinit {
-        baseAddress?.deallocate(capacity: count)
-    }
-
-    public var baseAddress: UnsafeMutablePointer<T>? {
-        return buffer.baseAddress
-    }
-
-    public var count: Int {
-        return buffer.count
-    }
-
-    public var rawBuffer: UnsafeMutableBufferPointer<T> {
-        return buffer
-    }
-
-    public subscript(index: Int) -> T {
-        get {
-            return buffer[index]
-        }
-        set {
-            buffer[index] = newValue
+        if freeWhenDone, let p = pointer {
+            p.deallocate(capacity: count)
         }
     }
 
-    private var buffer: UnsafeMutableBufferPointer<T>
+    public let pointer: UnsafeMutablePointer<T>?
+    public let count: Int
+    private let freeWhenDone: Bool
 }
--- Sources/HeapCString.swift
+++ Sources/HeapCString.swift
@@ -10,15 +10,6 @@ public typealias HeapCString = HeapBuffer<CChar>
 
 public extension HeapBuffer where T == CChar {
     public convenience init(string: String) {
-        let cstr = string.utf8CString
-
-        var pointer: UnsafePointer<CChar>?
-        var size: Int = 0
-        cstr.withUnsafeBufferPointer {
-            pointer = $0.baseAddress
-            size = $0.count
-        }
-
-        self.init(from: pointer, count: size)
+        self.init(items: string.utf8CString)
     }
 }
